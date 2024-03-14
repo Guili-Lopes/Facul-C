b12bed09289b8a850456c8d93805406292367b08
Guili-Lopes <143187163+Guili-Lopes@users.noreply.github.com>
2024-03-14 10:19:18 -0300

    Update ConversaoTempo.c

--- ConversaoTempo.c
+++ ConversaoTempo.c
@@ -1,3 +1,9 @@
+/*
+Leia um valor inteiro, que é o tempo de 
+duração em segundos de um determinado evento 
+em uma fábrica, e informe-o expresso no formato horas:minutos:segundos.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
